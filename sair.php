a78d0b4f418d3a5a60d35d0bcedcbb23fb060708
Gustavo Costa <gustavocsantosalmeida80@gmail.com>
2020-12-21 19:34:59 -0300

    Create sair.php

---
+++ sair.php
@@ -0,0 +1,19 @@
+<?
+session_start();
+// sesion_unset();
+session_destroy();
+ 
+//prontinho acima acabamos com todas as sessões existentes!
+echo "<font size=5><center><br><br><br>Voce saiu com sucesso!</center></font>";
+?>
+<!DOCTYPE html>
+<html>
+<body background="https://images.alphacoders.com/225/thumb-1920-225583.JPG" text="white">
+</form>
+</br>
+<p></p>
+<form method="POST" action="index.php" style="font-size:30px; text-align:center;">
+<input type="submit" value="Voltar" style="font-size:30px;">
+</form>
+</body>
+</html>
