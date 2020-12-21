319eb586a4e794524482ca36698d5f3926aa258a
Gustavo Costa <gustavocsantosalmeida80@gmail.com>
2020-12-21 19:33:27 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+  
+<html>
+<body background="https://images.alphacoders.com/225/thumb-1920-225583.JPG" text="white">
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<form method="POST" action="gravar.php" style="font-size:30px; text-align:center;">  Nome: 
+<input type="text" name="nome" size="30">
+</p>  <p>
+<input style="font-size:30px" type="submit" value="Enviar">
+</p>
+</form>
+</body>
+</html>
